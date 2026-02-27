b1c1b5116aa16e6f42471dc79ad3ee784b9b1a68
João Pedro Salatiel <contato.salatiel@hotmail.com>
2026-02-27 18:15:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 st.set_page_config(page_title="Customer Churn Dashboard", layout="wide")
 
 # Load data
-df = pd.read_csv("online_retail.csv", encoding="ISO-8859-1")
+df = pd.read_csv("churn_dashboard_data.csv", encoding="ISO-8859-1")
 
 st.title("📊 Customer Churn & Retention Strategy Dashboard")
 
